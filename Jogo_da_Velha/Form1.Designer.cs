793944a029aaad009df4360b56501fe2d75a7390
washingtoncosta <washingtongool@hotmail.com>
2017-06-01 20:40:03 -0300

    jogo

---
+++ Jogo_da_Velha/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Jogo_da_Velha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button21 = new System.Windows.Forms.Button();
+            this.button22 = new System.Windows.Forms.Button();
+            this.button23 = new System.Windows.Forms.Button();
+            this.button31 = new System.Windows.Forms.Button();
+            this.button32 = new System.Windows.Forms.Button();
+            this.button33 = new System.Windows.Forms.Button();
+            this.lb1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(63, 105);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(107, 104);
+            this.button11.TabIndex = 0;
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(196, 105);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(107, 104);
+            this.button12.TabIndex = 1;
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(327, 105);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(107, 104);
+            this.button13.TabIndex = 2;
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button21
+            // 
+            this.button21.Location = new System.Drawing.Point(63, 242);
+            this.button21.Name = "button21";
+            this.button21.Size = new System.Drawing.Size(107, 104);
+            this.button21.TabIndex = 5;
+            this.button21.UseVisualStyleBackColor = true;
+            this.button21.Click += new System.EventHandler(this.button21_Click);
+            // 
+            // button22
+            // 
+            this.button22.Location = new System.Drawing.Point(196, 242);
+            this.button22.Name = "button22";
+            this.button22.Size = new System.Drawing.Size(107, 104);
+            this.button22.TabIndex = 4;
+            this.button22.UseVisualStyleBackColor = true;
+            this.button22.Click += new System.EventHandler(this.button22_Click);
+            // 
+            // button23
+            // 
+            this.button23.Location = new System.Drawing.Point(327, 242);
+            this.button23.Name = "button23";
+            this.button23.Size = new System.Drawing.Size(107, 104);
+            this.button23.TabIndex = 3;
+            this.button23.UseVisualStyleBackColor = true;
+            this.button23.Click += new System.EventHandler(this.button23_Click);
+            // 
+            // button31
+            // 
+            this.button31.Location = new System.Drawing.Point(63, 377);
+            this.button31.Name = "button31";
+            this.button31.Size = new System.Drawing.Size(107, 104);
+            this.button31.TabIndex = 8;
+            this.button31.UseVisualStyleBackColor = true;
+            this.button31.Click += new System.EventHandler(this.button31_Click);
+            // 
+            // button32
+            // 
+            this.button32.Location = new System.Drawing.Point(196, 377);
+            this.button32.Name = "button32";
+            this.button32.Size = new System.Drawing.Size(107, 104);
+            this.button32.TabIndex = 7;
+            this.button32.UseVisualStyleBackColor = true;
+            this.button32.Click += new System.EventHandler(this.button32_Click);
+            // 
+            // button33
+            // 
+            this.button33.Location = new System.Drawing.Point(327, 377);
+            this.button33.Name = "button33";
+            this.button33.Size = new System.Drawing.Size(107, 104);
+            this.button33.TabIndex = 6;
+            this.button33.UseVisualStyleBackColor = true;
+            this.button33.Click += new System.EventHandler(this.button33_Click);
+            // 
+            // lb1
+            // 
+            this.lb1.AutoSize = true;
+            this.lb1.ForeColor = System.Drawing.Color.White;
+            this.lb1.Location = new System.Drawing.Point(205, 72);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(80, 13);
+            this.lb1.TabIndex = 9;
+            this.lb1.Text = "GO GO GO GO";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(57, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(377, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Alexandre Queiros de Oliveira RA 2215111907";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(57, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(377, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Washington Costa RA 2215112179";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(502, 514);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lb1);
+            this.Controls.Add(this.button31);
+            this.Controls.Add(this.button32);
+            this.Controls.Add(this.button33);
+            this.Controls.Add(this.button21);
+            this.Controls.Add(this.button22);
+            this.Controls.Add(this.button23);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button21;
+        private System.Windows.Forms.Button button22;
+        private System.Windows.Forms.Button button23;
+        private System.Windows.Forms.Button button31;
+        private System.Windows.Forms.Button button32;
+        private System.Windows.Forms.Button button33;
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Jogo_da_Velha/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Jogo_da_Velha
+{
+    public partial class Form1 : Form
+    {
+
+        bool x = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            button11.Click += new EventHandler(buttonClick);
+            button12.Click += new EventHandler(buttonClick);
+            button13.Click += new EventHandler(buttonClick);
+            button21.Click += new EventHandler(buttonClick);
+            button22.Click += new EventHandler(buttonClick);
+            button23.Click += new EventHandler(buttonClick);
+            button31.Click += new EventHandler(buttonClick);
+            button32.Click += new EventHandler(buttonClick);
+            button33.Click += new EventHandler(buttonClick);
+
+            foreach (Control item in this.Controls)
+            {
+
+                if (item is Button)
+                {
+
+                    item.TabStop = false;
+                }
+            }
+
+
+        }
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+
+            ((Button)sender).Text = this.x ? "X" : "O";
+            ((Button)sender).Enabled = false;
+
+            Ganhador();
+
+            x = !x;
+
+            lb1.Text = String.Format("{0}, sua vez", this.x ? "x" : "O");
+        }
+
+        private void Ganhador()
+        {
+            if (
+
+                    button11.Text != String.Empty && button11.Text == button12.Text && button12.Text == button13.Text ||
+                    button21.Text != String.Empty && button21.Text == button22.Text && button22.Text == button23.Text ||
+                    button31.Text != String.Empty && button31.Text == button32.Text && button32.Text == button33.Text ||
+
+                    button11.Text != String.Empty && button11.Text == button21.Text && button21.Text == button31.Text ||
+                    button12.Text != String.Empty && button12.Text == button22.Text && button22.Text == button32.Text ||
+                    button13.Text != String.Empty && button13.Text == button23.Text && button23.Text == button33.Text ||
+
+                    button11.Text != String.Empty && button11.Text == button22.Text && button22.Text == button33.Text ||
+                    button13.Text != String.Empty && button13.Text == button22.Text && button22.Text == button31.Text
+                )
+            {
+                MessageBox.Show(String.Format("Ganhador é o [{0}]", x ? "x" : "O"), " Parabens ao Ganhador", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                Recomecar();
+
+            }
+            else
+            {
+                Empate();
+            }
+
+
+        }
+
+        private void Recomecar()
+        {
+            foreach (Control item in this.Controls)
+            {
+                if (item is Button)
+                {
+
+                    item.Enabled = true;
+                    item.Text = String.Empty;
+                }
+            }
+
+        }
+
+        private void Empate()
+        {
+            bool desabilitados = true;
+
+            foreach (Control item in this.Controls)
+            {
+
+                if (item is Button && item.Enabled)
+                {
+                    desabilitados = false;
+                    break;
+                }
+            }
+            if (desabilitados)
+            {
+                MessageBox.Show(String.Format("Empate"), "Empate", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                Recomecar();
+            }
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            ((Button)sender).Text = this.x ? "X" : "O";
+            ((Button)sender).Enabled = false;
+
+            Ganhador();
+
+            x = !x;
+
+            lb1.Text = String.Format("{0}, sua vez", this.x ? "x" : "O");
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            ((Button)sender).Text = this.x ? "X" : "O";
+            ((Button)sender).Enabled = false;
+
+            Ganhador();
+
+            x = !x;
+
+            lb1.Text = String.Format("{0}, sua vez", this.x ? "x" : "O");
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            ((Button)sender).Text = this.x ? "X" : "O";
+            ((Button)sender).Enabled = false;
+
+            Ganhador();
+
+            x = !x;
+
+            lb1.Text = String.Format("{0}, sua vez", this.x ? "x" : "O");
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            ((Button)sender).Text = this.x ? "X" : "O";
+            ((Button)sender).Enabled = false;
+
+            Ganhador();
+
+            x = !x;
+
+            lb1.Text = String.Format("{0}, sua vez", this.x ? "x" : "O");
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            ((Button)sender).Text = this.x ? "X" : "O";
+            ((Button)sender).Enabled = false;
+
+            Ganhador();
+
+            x = !x;
+
+            lb1.Text = String.Format("{0}, sua vez", this.x ? "x" : "O");
+        }
+
+        private void button23_Click(object sender, EventArgs e)
+        {
+            ((Button)sender).Text = this.x ? "X" : "O";
+            ((Button)sender).Enabled = false;
+
+            Ganhador();
+
+            x = !x;
+
+            lb1.Text = String.Format("{0}, sua vez", this.x ? "x" : "O");
+        }
+
+        private void button31_Click(object sender, EventArgs e)
+        {
+            ((Button)sender).Text = this.x ? "X" : "O";
+            ((Button)sender).Enabled = false;
+
+            Ganhador();
+
+            x = !x;
+
+            lb1.Text = String.Format("{0}, sua vez", this.x ? "x" : "O");
+        }
+
+        private void button32_Click(object sender, EventArgs e)
+        {
+            ((Button)sender).Text = this.x ? "X" : "O";
+            ((Button)sender).Enabled = false;
+
+            Ganhador();
+
+            x = !x;
+
+            lb1.Text = String.Format("{0}, sua vez", this.x ? "x" : "O");
+        }
+
+        private void button33_Click(object sender, EventArgs e)
+        {
+            ((Button)sender).Text = this.x ? "X" : "O";
+            ((Button)sender).Enabled = false;
+
+            Ganhador();
+
+            x = !x;
+
+            lb1.Text = String.Format("{0}, sua vez", this.x ? "x" : "O");
+        }
+    }
+
+
+}
